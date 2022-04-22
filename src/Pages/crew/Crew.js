d17cdda859de12ebbd0e9e08cdc294bcba3a9bb9
Joseph Salac <jsalac@deloitte.com>
2022-04-22 09:54:21 +0800

    polished ui in crew page

--- src/Pages/crew/Crew.js
+++ src/Pages/crew/Crew.js
@@ -20,7 +20,6 @@ export default function Crew() {
 
   const scrollToImage = (index) => {
     setSelectedCrew(index);
-    console.log(index);
     refs[index].current.scrollIntoView({
       behavior: "smooth",
       block: "nearest",
@@ -51,7 +50,7 @@ export default function Crew() {
           <div className="flex overflow-x-hidden mt-10">
             {content.map((item, index) => (
               <div
-                className="border-b border-gray-500 md:hidden flex flex-shrink-0 w-full"
+                className="border-b border-gray-500 md:hidden flex flex-shrink-0 w-full "
                 key={index}
                 ref={refs[index]}
               >
@@ -64,7 +63,7 @@ export default function Crew() {
             ))}
           </div>
         </div>
-        <div className="lg:flex lg:h-full">
+        <div className="md:h-full md:flex md:flex-col md:justify-between lg:flex lg:flex-row lg:h-full  ">
           <div className=" lg:w-1/2">
             <div className="hidden lg:flex  lg:h-2/5 lg:items-center lg:pt-[2px] lg:pl-[20%]">
               <h2 className="font-thin font-barlow-condensed lg:text-[28px] text-white lg:tracking-widest">
@@ -74,19 +73,19 @@ export default function Crew() {
                 MEET YOUR CREW
               </h2>
             </div>
-            <div className="md:flex md:flex-col-reverse mt-2  lg:mt-0  lg:h-3/5 lg:pl-[20%]">
+            <div className="md:flex md:flex-col-reverse mt-2  lg:mt-0  lg:h-3/5 lg:pl-[20%] ">
               <div className="flex justify-center  h-10  items-center lg:justify-start  lg:h-1/2 lg:items-start ">
                 <ul
                   className="flex justify-evenly w-1/2 
-                        md:text-[19px]  md:justify-evenly md:px-0 md:w-1/3  lg:justify-start lg:space-x-8"
+                        md:text-[19px]  md:justify-evenly md:px-0 md:w-1/3  lg:justify-start lg:space-x-8  items-center"
                 >
                   {navs.map((nav, index) => (
                     <li
                       key={nav}
-                      className={` w-3 h-3  rounded-full
+                      className={` w-3 h-3 md:h-4 md:w-4   rounded-full cursor-pointer hover:h-5 hover:w-5
                   ${
                     nav === content[selectedCrew].id
-                      ? "bg-gray-200"
+                      ? "bg-gray-200 h-5 w-5 md:h-6 md:w-6"
                       : "bg-gray-700"
                   }
                 `}
@@ -95,8 +94,8 @@ export default function Crew() {
                   ))}
                 </ul>
               </div>
-              <div className=" h-1/3 md:h-full flex flex-col justify-between">
-                <div className="mt-[20px]">
+              <div className=" h-1/3 md:h-full flex flex-col justify-between pb-4 space-y-4">
+                <div className="pt-[20px] space-y-2">
                   <h1 className="text-white  text-center text-[14px] font-bellefair tracking-[2px] md:text-[24px] lg:text-left lg:text-[32px]">
                     {content[selectedCrew].position.toUpperCase()}
                   </h1>
@@ -112,17 +111,17 @@ export default function Crew() {
               </div>
             </div>
           </div>
-          <div className="hidden md:w-full md:flex overflow-x-hidden   lg:w-1/2 lg:items-end ">
+          <div className="hidden md:w-full md:flex overflow-hidden lg:h-full   lg:w-1/2 lg:items-end  ">
             {content.map((item, index) => (
               <div
-                className="w-full flex-shrink-0 "
+                className="flex w-full lg:h-[90%]  flex-shrink-0 overflow-hidden  "
                 key={index}
                 ref={refsTablet[index]}
               >
                 <img
                   src={content[index].photo}
                   alt={content[index].about}
-                  className="h-[280px] md:h-[572px] md:w-full  object-contain"
+                  className=" h-[280px] md:h-[572px] md:w-full md:object-scale-down lg:w-auto lg:h-auto lg:object-fill  "
                 />
               </div>
             ))}
