5bcbde493604bebbff68e7b83a76d6396a1bb216
Joseph Salac <jsalac@deloitte.com>
2022-04-22 13:43:51 +0800

    enlarge the planet image on the desktop view of at the destination page

--- src/Pages/destination/Destination.js
+++ src/Pages/destination/Destination.js
@@ -26,8 +26,8 @@ export default function Destination() {
     <div
       className={`h-full bg-gray-500 pt-[88px]  ${bgImage} overflow-auto flex flex-col justify-center lg:flex-row lg:items-center lg:pb-20`}
     >
-      <div className="text-white  lg:w-3/6 lg:h-full">
-        <div className="flex justify-center font-barlow-condensed tracking-[2px] text-[16px] md:text-[20px] md:justify-start md:pl-[38px] lg:text-[28px] lg:h-1/4 lg:px-36 lg:items-end  ">
+      <div className="text-white  lg:w-3/6 lg:h-full ">
+        <div className="flex justify-center font-barlow-condensed tracking-[2px] text-[16px] md:text-[20px] md:justify-start md:pl-[38px] lg:text-[28px]  lg:px-16 lg:items-end lg:h-32  ">
           <h2 className="font-thin ">
             <span className="font-bold text-gray-600">
               {content[selectedPlanet].id}{" "}
@@ -35,27 +35,27 @@ export default function Destination() {
             PICK YOUR DESTINATION
           </h2>
         </div>
-        <div className="w-full flex  mt-[32px]  lg:h-3/4 lg:items-center  overflow-hidden">
+        <div className="w-full flex  mt-[32px] lg:mt-0 lg:items-center   overflow-hidden  lg:h-auto lg:p-8">
           {content.map((planet, index) => (
             <div
               key={index}
-              className="w-full flex justify-center flex-shrink-0"
+              className="w-full flex justify-center flex-shrink-0 "
               ref={refs[index]}
             >
               <img
                 src={planet.img}
                 alt={planet.destination}
-                className="w-[170px] h-[170px] md:w-[300px] md:h-[300px] lg:w-[445px] lg:h-[445px]"
+                className="w-[170px] h-[170px] md:w-[300px] md:h-[300px] lg:w-[80%] lg:h-auto lg:object-center"
               />
             </div>
           ))}
         </div>
       </div>
 
-      <div className="lg:w-1/3">
+      <div className="lg:w-1/3  lg:pl-10 ">
         <div className="md:flex md:justify-center  lg:justify-start ">
           <ul
-            className="flex justify-around px-[69px] text-white font-thin font-barlow-condensed text-[16px] mt-[26px] md:text-[26px] lg:text-[30px] 
+            className="flex justify-around px-[69px] text-white font-thin font-barlow-condensed text-[16px] mt-[26px] md:text-[26px] lg:text-[26px] 
             md:justify-evenly md:px-0 md:w-1/2
             lg:px-0 lg:justify-start lg:space-x-[35px]"
           >
@@ -77,17 +77,17 @@ export default function Destination() {
             ))}
           </ul>
         </div>
-        <div className="mt-[20px]">
-          <h1 className="text-white  text-center text-[56px] font-bellefair tracking-[2px] md:text-[80px] lg:text-left">
+        <div className="mt-[20px] ">
+          <h1 className="text-white  text-center text-[56px] font-bellefair tracking-[2px] md:text-[80px] lg:text-[130px] lg:text-left">
             {content[selectedPlanet].destination}
           </h1>
         </div>
-        <div className="px-[24px] md:px-[97px] lg:px-0  lg:pr-44">
+        <div className="px-[24px] md:px-[97px] lg:px-0  lg:pr-10">
           <p className="text-desc text-center font-barlow font-thin text-[15px] px-[24px] md:text-[20px] lg:text-[24px] lg:text-left lg:px-0">
             {content[selectedPlanet].description}
           </p>
         </div>
-        <div className="px-[24px] mt-10 md:px-[97px] lg:px-0 lg:pr-44">
+        <div className="px-[24px] mt-10 md:px-[97px] lg:px-0 lg:pr-12">
           <div className="w-full border-b border-gray-600"></div>
         </div>
         <div
